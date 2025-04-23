2a9c98c82bdb43209ed3522bb51c5fb676752ffe
nebence <nebence12@gmail.com>
2025-04-23 18:55:22 +0200

    get completed+incompleted forms

--- PitchforkForms_Backend/routes/userActions.js
+++ PitchforkForms_Backend/routes/userActions.js
@@ -85,4 +85,58 @@ router.delete("/users/:id", authenticateToken, isAdmin, async (req, res) => {
     }
 });
 
+// GET INCOMPLETE FORMS FOR USER
+router.get("/users/:id/forms/incomplete", async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const sql = `
+            SELECT f.id, f.title, f.description
+            FROM forms f
+            LEFT JOIN form_submissions fs ON f.id = fs.form_id AND fs.user_id = ?
+            WHERE fs.id IS NULL
+        `;
+        const results = await dbQuery(sql, [userId]);
+
+        
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Nincs kitöltetlen form!" });
+        }
+
+        res.json(results);
+    } catch (err) {
+        console.error("DB hiba:", err);
+        res.status(500).json({ message: "Szerverhiba." });
+    }
+});
+
+  
+// GET COMPLETED FORMS FOR USER
+router.get("/users/:id/forms/completed", async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const sql = `
+            SELECT f.id, f.title, f.description, fs.submitted_at
+            FROM forms f
+            INNER JOIN form_submissions fs ON f.id = fs.form_id
+            WHERE fs.user_id = ?
+        `;
+        const results = await dbQuery(sql, [userId]);
+
+       
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Nincs kitöltött form!" });
+        }
+
+        res.json(results);
+    } catch (err) {
+        console.error("DB hiba:", err);
+        res.status(500).json({ message: "Szerverhiba." });
+    }
+});
+
+
+
+
 module.exports = router
